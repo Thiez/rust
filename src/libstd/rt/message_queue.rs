daddc03357850a328c11f93179e72dd470f91524
Matthijs Hofstra <thiezz@gmail.com>
2013-09-02 12:09:52 +0200

    Swapped mesage_queue implementation by a better one by Dmitriy Vyukov

--- src/libstd/rt/message_queue.rs
+++ src/libstd/rt/message_queue.rs
@@ -8,79 +8,73 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-//! A concurrent queue that supports multiple producers and a
-//! single consumer.
+//! An efficient lock-free multiple-producer/single-consumer queue as described by Dmitriy Vyukov
+//! Original version (in C) can be found here:
+//! http://www.1024cores.net/home/lock-free-algorithms/queues/non-intrusive-mpsc-node-based-queue
 
-use container::Container;
-use kinds::Send;
-use vec::OwnedVector;
-use cell::Cell;
-use option::*;
-use unstable::sync::{UnsafeArc, LittleLock};
+use cast::transmute;
 use clone::Clone;
+use ops::Drop;
+use kinds::Send;
+use option::{Option,Some,None};
+use ptr::{is_not_null, mut_null};
+use unstable::atomics::{AtomicPtr,AcqRel,Acquire,Release};
+use unstable::sync::UnsafeArc;
+use util::swap;
 
+/// A lockless concurrent Queue.
 pub struct MessageQueue<T> {
-    priv state: UnsafeArc<State<T>>
+    priv state: UnsafeArc<State<T>>,
 }
 
+#[mutable]
 struct State<T> {
-    count: uint,
-    queue: ~[T],
-    lock: LittleLock
+    head: AtomicPtr<Node<T>>,
+    tail: *mut Node<T>,
 }
 
 impl<T: Send> MessageQueue<T> {
     pub fn new() -> MessageQueue<T> {
-        MessageQueue {
-            state: UnsafeArc::new(State {
-                count: 0,
-                queue: ~[],
-                lock: LittleLock::new()
-            })
+            // the sentinel node
+            let node = unsafe { transmute::<~Node<T>,*mut Node<T>>(~Node::new()) };
+            MessageQueue {
+                state: UnsafeArc::new(State {
+                    head: AtomicPtr::new(node),
+                    tail: node
+                })
+            }
+    }
+
+    pub fn push(&self, value: T) {
+        unsafe {
+            let state = self.state.get();
+            let node = transmute::<~Node<T>,*mut Node<T>>(~Node::new());
+            (*node).value = Some(value);
+            let prev = (*state).head.swap(node, AcqRel);
+            (*prev).next.store(node, Release);
         }
     }
 
-    pub fn push(&mut self, value: T) {
+    pub fn pop(&self) -> Option<T> {
         unsafe {
-            let value = Cell::new(value);
             let state = self.state.get();
-            do (*state).lock.lock {
-                (*state).count += 1;
-                (*state).queue.push(value.take());
+            let tail = (*state).tail;
+            let next = (*tail).next.load(Acquire);
+            if is_not_null(next) {
+                (*state).tail = next;
+                let mut value = None;
+                swap(&mut value, &mut (*next).value);   // Get the value.
+                let _: ~Node<T> = transmute(tail); // Let drop glue handle the tail.
+                value
+            } else {
+                None
             }
         }
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        unsafe {
-            let state = self.state.get();
-            do (*state).lock.lock {
-                if !(*state).queue.is_empty() {
-                    (*state).count += 1;
-                    Some((*state).queue.shift())
-                } else {
-                    None
-                }
-            }
-        }
-    }
-
-    /// A pop that may sometimes miss enqueued elements, but is much faster
-    /// to give up without doing any synchronization
-    pub fn casual_pop(&mut self) -> Option<T> {
-        unsafe {
-            let state = self.state.get();
-            // NB: Unsynchronized check
-            if (*state).count == 0 { return None; }
-            do (*state).lock.lock {
-                if !(*state).queue.is_empty() {
-                    (*state).count += 1;
-                    Some((*state).queue.shift())
-                } else {
-                    None
-                }
-            }
-        }
+    #[inline]
+    pub fn casual_pop(&self) -> Option<T> {
+        self.pop()
     }
 }
 
@@ -91,3 +85,33 @@ impl<T: Send> Clone for MessageQueue<T> {
         }
     }
 }
+
+#[unsafe_destructor]
+impl<T: Send> Drop for State<T> {
+    fn drop(&self) {
+        unsafe {
+            let mut node = (*self).tail;
+            while is_not_null(node) {
+                let next = (*node).next.load(Acquire);
+                let _: ~Node<T> = transmute(node);  // Destroy node and contents.
+                node = next;
+            }
+        }
+    }
+}
+
+struct Node<T> {
+    next: AtomicPtr<Node<T>>,
+    value: Option<T>,
+}
+
+impl<T: Send> Node<T> {
+    #[inline]
+    fn new() -> Node<T> {
+        Node {
+            next: AtomicPtr::new( mut_null() ),
+            value: None,
+        }
+    }
+}
+
